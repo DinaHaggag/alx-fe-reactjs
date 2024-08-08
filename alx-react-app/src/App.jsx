14d26de6d9dc72b08450f058cfef080e4ce17d67
DinaHaggag <dina.m.hagag@gmail.com>
2024-08-08 13:57:50 +0300

    Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,16 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import './App.css'
 
 function App() {
   return (
       <div className="App">
           <WelcomeMessage />
+          <Header />
+          <MainContent />
+          <Footer />
       </div>
   );
 }
