25f42fe89c14633a5e097866afd735014cf7fcf3
DinaHaggag <dina.m.hagag@gmail.com>
2024-08-18 02:30:23 +0300

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+      <main style={{ color: 'white', textAlign: 'center', fontWeight:"bold" }}>
+          <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+  );
+}
+
+export default MainContent;
