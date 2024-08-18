9d8aa75620d885e73bbcfef7b0b366747b4cc61a
DinaHaggag <dina.m.hagag@gmail.com>
2024-08-18 13:08:41 +0300

    navbar styling

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const navStyle = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    padding: '10px',
+    backgroundColor: '#333',
+    color: 'white',
+  };
+
+  const linkStyle = {
+    color: 'white',
+    textDecoration: 'none',
+  };
+
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
-      <Link to="/" style={{ margin: '0 10px', color: '#fff' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 10px', color: '#fff' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 10px', color: '#fff' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '0 10px', color: '#fff' }}>Contact</Link>
+    <nav style={navStyle}>
+      <Link to="/" style={linkStyle}>Home</Link>
+      <Link to="/about" style={linkStyle}>About</Link>
+      <Link to="/services" style={linkStyle}>Services</Link>
+      <Link to="/contact" style={linkStyle}>Contact</Link>
     </nav>
   );
 }
