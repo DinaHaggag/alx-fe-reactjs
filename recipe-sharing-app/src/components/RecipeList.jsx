556b9903ed58de804fee6a00935060b4295f9733
DinaHaggag <dina.m.hagag@gmail.com>
2024-08-24 01:26:30 +0300

    Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,15 +1,18 @@
 
-import useRecipeStore from './recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
+  const recipes = useRecipeStore((state) => state.filteredRecipes.length > 0 ? state.filteredRecipes : state.recipes);
 
   return (
     <div>
-      {recipes.map(recipe => (
+      {recipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
+          {/* Use Link to navigate to the RecipeDetails page */}
+          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
         </div>
       ))}
     </div>
