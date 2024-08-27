0a050e2d4acd2a373180836875e1ea7e0870d723
DinaHaggag <dina.m.hagag@gmail.com>
2024-08-27 13:09:00 +0300

    add AddRecipeForm

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
@@ -14,6 +15,7 @@ function App() {
         <RecommendationsList />
         <Routes>
           <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
         </Routes>
       </div>
