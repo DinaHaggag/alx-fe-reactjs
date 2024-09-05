993566fe23083ecca7403a67392199b6638c3dc9
DinaHaggag <dina.m.hagag@gmail.com>
2024-09-06 02:46:12 +0300

    add dark mode

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,11 @@
 // tailwind.config.js
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // This ensures Tailwind checks for classes inside your components.
+  
+  darkMode: 'class', // Optional. If you don't need dark mode, you can remove this.
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
