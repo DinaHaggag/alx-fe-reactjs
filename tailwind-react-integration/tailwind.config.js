3fb067187db44b60731da74d7f3bb8c87c800ae8
DinaHaggag <dina.m.hagag@gmail.com>
2024-09-06 02:41:13 +0300

    error handling

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,6 @@
 // tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // This ensures Tailwind checks for classes inside your components.
   theme: {
     extend: {},
   },
