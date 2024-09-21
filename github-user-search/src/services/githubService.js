9d4e930f15a899d5c493405ce06dbc5c425ad128
DinaHaggag <dina.m.hagag@gmail.com>
2024-09-22 02:36:53 +0300

    advanced integration

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -15,7 +15,7 @@ export const searchUsers = async ({ username, location, minRepos }) => {
   const query = `q=${username}${location ? `+location:${location}` : ''}${minRepos ? `+repos:>=${minRepos}` : ''}`;
 
   try {
-    const response = await axios.get(`${GITHUB_API_BASE_URL}/search/users?${query}`);
+    const response = await axios.get(`https://ssss${GITHUB_API_BASE_URL}/search/users?${query}`);
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch users from GitHub API');
