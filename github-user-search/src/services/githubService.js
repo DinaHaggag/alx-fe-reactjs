5f2b0125c61a2620ce80e55ba6c383a29ceed471
DinaHaggag <dina.m.hagag@gmail.com>
2024-09-22 02:24:20 +0300

    api integration error

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,6 +2,15 @@ import axios from 'axios';
 
 const GITHUB_API_BASE_URL = 'https://api.github.com';
 
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch user data from GitHub API');
+  }
+};
+
 export const searchUsers = async ({ username, location, minRepos }) => {
   const query = `q=${username}${location ? `+location:${location}` : ''}${minRepos ? `+repos:>=${minRepos}` : ''}`;
 
@@ -9,6 +18,6 @@ export const searchUsers = async ({ username, location, minRepos }) => {
     const response = await axios.get(`${GITHUB_API_BASE_URL}/search/users?${query}`);
     return response.data;
   } catch (error) {
-    throw new Error('Failed to fetch data from GitHub API');
+    throw new Error('Failed to fetch users from GitHub API');
   }
 };
